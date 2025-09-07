bb4f191818ed8331dc5702190ab2ac1a04a03264
Teodora <teavuckovic17@gmail.com>
2025-09-07 20:26:08 +0200

    Coupon model, routes, controller

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
+import couponRoutes from "./routes/coupon.route.js"
 import { connectDB } from "./lib/db.js";
 
 dotenv.config(); // da mozemo da citamo zadrzaj .env fajla
@@ -18,6 +19,7 @@ app.use(cookieParser()); //da moze da se pristupi cookijima
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
