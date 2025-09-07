1124e62c90050c9105d1fa657d46d0768c6ab003
Teodora <teavuckovic17@gmail.com>
2025-09-07 20:05:18 +0200

    Cart routes, controller

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
+import cartRoutes from "./routes/cart.route.js"
 import { connectDB } from "./lib/db.js";
 
 dotenv.config(); // da mozemo da citamo zadrzaj .env fajla
@@ -16,6 +17,7 @@ app.use(cookieParser()); //da moze da se pristupi cookijima
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
