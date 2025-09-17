11d51c4413c30c9f2369df20ca4d977658568dd0
Teodora <teavuckovic17@gmail.com>
2025-09-17 09:42:11 +0200

    Dodatne metode zastite: rate limiting, helmet i cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
@@ -9,6 +11,7 @@ import cartRoutes from "./routes/cart.route.js"
 import couponRoutes from "./routes/coupon.route.js"
 import orderRoutes from "./routes/order.route.js"
 import { connectDB } from "./lib/db.js";
+import { rateLimiter } from "./lib/rateLimit.js";
 
 dotenv.config(); // da mozemo da citamo zadrzaj .env fajla
 
@@ -17,6 +20,12 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json()); // da moze da se parsira body od request-a
 app.use(cookieParser()); //da moze da se pristupi cookijima
+app.use(helmet());
+app.use(rateLimiter);
+app.use(cors({
+  origin: "http://localhost:3000", // samo frontend sa ovog domena moze da pristupa ovom API-ju
+  credentials: true, // za cookies
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
